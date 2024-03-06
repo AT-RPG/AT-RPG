146bf021bcc7d7bd3313688b3090db65994f43a4
Jiwon <morex1566@gmail.com>
2024-03-07 04:33:23 +0900

    [2024-03-07] 버그 수정 : AssetBundle에 등록된 리소스가 타입은 다르지만, 이름이 같은 경우, 리소스 매핑이 사라지는 문제

--- Assets/Scripts/00_Common/Managers/Management.cs
+++ Assets/Scripts/00_Common/Managers/Management.cs
@@ -1,10 +1,16 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityObject = UnityEngine.Object;
 
 namespace AT_RPG.Manager
 {
+    // Key1 = 씬 이름, Value1 = 씬에서 사용될 리소스 매핑s
     public class SceneResourceMap : Dictionary<string, ResourceMap> { }
-    public class ResourceMap : Dictionary<string, Object> { }
+
+    // Key1 = 리소스의 타입, Key2 = 리소스의 이름, Value1 = 리소스
+    public class ResourceMap : Dictionary<string, Dictionary<string, UnityObject>> { }
+
+    // Key1 = 씬 이름, Value1 = 씬에 적용되는 리소스 번들s
     public class AssetBundleMap : Dictionary<string, List<AssetBundle>> { }
 }
