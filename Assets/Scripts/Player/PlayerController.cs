0d2e2139ccb8b7a99b7546639b70701cccf05d2d
IJH_97 <dryer97@naver.com>
2024-03-16 10:12:50 +0900

    [2024-03-16] 브랜치 생성전 커밋
    무기 이펙트 실험중

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : CharacterProperty
     [SerializeField] private Transform characterBody;
     [SerializeField] private Transform cameraArm;
     [SerializeField] private Rigidbody playerRigid;
+    [SerializeField] private float moveSpeed = 3.0f;
     // [SerializeField] Transform myCam;
 
     // public LayerMask crashMask;
@@ -15,6 +16,9 @@ public class PlayerController : CharacterProperty
     // float targetDist;
     // float camDist;
 
+    /// <summary>
+    /// 입력 키 추가
+    /// </summary>
     void Awake()
     {
         InputManager.AddKeyAction("Dodge", Dodge);
@@ -34,6 +38,9 @@ public class PlayerController : CharacterProperty
     {
     }
 
+    /// <summary>
+    /// 플레이어의 이동
+    /// </summary>
     private void Move(InputValue value)
     {
         if(myAnim.GetBool("isRolling")) return;
@@ -48,11 +55,14 @@ public class PlayerController : CharacterProperty
             Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
 
             characterBody.forward = moveDir;
-            transform.position += moveDir * Time.deltaTime * 3.0f;
+            transform.position += moveDir * Time.deltaTime * moveSpeed;
         }
         Debug.DrawRay(cameraArm.position, new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized, Color.red);
     }
 
+    /// <summary>
+    /// 카메라 시선 처리
+    /// </summary>
     private void LookAround(InputValue value)
     {
         Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
@@ -81,6 +91,9 @@ public class PlayerController : CharacterProperty
         // myCam.localPosition = new Vector3(0, 0, -camDist);
     }
 
+    /// <summary>
+    /// 회피
+    /// </summary>
     private void Dodge(InputValue value)
     {
         if(myAnim.GetBool("isJumping")) return;
@@ -89,6 +102,9 @@ public class PlayerController : CharacterProperty
         myAnim.SetBool("isRolling", true);
     }
 
+    /// <summary>
+    /// 점프
+    /// </summary>
     private void Jump(InputValue value)
     {
         if(myAnim.GetBool("isRolling") || myAnim.GetBool("isJumping")) return;
@@ -98,6 +114,17 @@ public class PlayerController : CharacterProperty
         playerRigid.AddForce(Vector3.up * 200.0f);
     }
 
+    /// <summary>
+    /// 지면을 감지하는 값을 받아 처리하는 함수
+    /// </summary>
+    public void SetFallState(bool isGround)
+    {
+        myAnim.SetBool("isGround", isGround);
+    }
+
+    /// <summary>
+    /// 오브젝트 삭제시(ex:씬 이동) 추가했던 키 제거 (중복방지)
+    /// </summary>
     private void OnDestroy() 
     {
         InputManager.RemoveKeyAction("Dodge", Dodge);
@@ -106,9 +133,5 @@ public class PlayerController : CharacterProperty
         InputManager.RemoveKeyAction("Move Left/Move Right", Move);
         InputManager.RemoveKeyAction("Aim", LookAround);
     }
-
-    public void SetFallState(bool isGround)
-    {
-        myAnim.SetBool("isGround", isGround);
-    }
 }
+
