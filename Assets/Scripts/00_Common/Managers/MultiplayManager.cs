88ff4c5731503f43b40f5cc14112646857e642a8
Jiwon <morex1566@gmail.com>
2024-03-19 18:35:24 +0900

    [2024-03-19] 클래스 추가 : 멀티플레이 매니저 생성

---
+++ Assets/Scripts/00_Common/Managers/MultiplayManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AT_RPG.Manager
+{
+    /// <summary>
+    /// 멀티플레이와 관련된 모든 기능을 관리합니다.
+    /// </summary>
+    public class MultiplayManager : Singleton<MultiplayManager>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+    }
+}
