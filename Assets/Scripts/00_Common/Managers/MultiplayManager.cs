0df2dd2fe6bd19bfe1f0f4fb5b3ff5ccd4c6d6ec
Jiwon <morex1566@gmail.com>
2024-03-19 18:49:07 +0900

    [2024-03-19] 클래스 추가 : 멀티플레이 매니저 설정용 스크립터블 오브젝트

--- Assets/Scripts/00_Common/Managers/MultiplayManager.cs
+++ Assets/Scripts/00_Common/Managers/MultiplayManager.cs
@@ -7,11 +7,20 @@ namespace AT_RPG.Manager
     /// <summary>
     /// 멀티플레이와 관련된 모든 기능을 관리합니다.
     /// </summary>
-    public class MultiplayManager : Singleton<MultiplayManager>
+    public partial class MultiplayManager : Singleton<MultiplayManager>
     {
+        private static MultiplayManagerSetting setting;
+
         protected override void Awake()
         {
             base.Awake();
+
+            setting = Resources.Load<MultiplayManagerSetting>("MultiplayManagerSettings");
         }
+    }
+
+    public partial class MultiplayManager
+    {
+        public static MultiplayManagerSetting Setting => setting;
     }
 }
