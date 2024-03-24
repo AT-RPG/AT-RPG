6550af6ca9fb33cd35f757ebe00d0670c1e7c348
Jiwon <morex1566@gmail.com>
2024-03-24 20:10:16 +0900

    [2024-03-24] 기능 수정 : 테스트 멀티플레이 객체 비활성화

--- Assets/Scripts/Multiplay/MultiplayNetworkRunner.cs
+++ Assets/Scripts/Multiplay/MultiplayNetworkRunner.cs
@@ -106,14 +106,14 @@ namespace AT_RPG
 
         void INetworkRunnerCallbacks.OnPlayerJoined(NetworkRunner runner, PlayerRef player) 
         {
-            if (runner.IsServer)
-            {
-                // Create a unique position for the player
-                Vector3 spawnPosition = new Vector3((player.RawEncoded % runner.Config.Simulation.DefaultPlayers) * 3, 1, 0);
-                NetworkObject networkPlayerObject = runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, player);
-                // Keep track of the player avatars so we can remove it when they disconnect
-                _spawnedCharacters.Add(player, networkPlayerObject);
-            }
+            //if (runner.IsServer)
+            //{
+            //    // Create a unique position for the player
+            //    Vector3 spawnPosition = new Vector3((player.RawEncoded % runner.Config.Simulation.DefaultPlayers) * 3, 1, 0);
+            //    NetworkObject networkPlayerObject = runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, player);
+            //    // Keep track of the player avatars so we can remove it when they disconnect
+            //    _spawnedCharacters.Add(player, networkPlayerObject);
+            //}
         }
 
         void INetworkRunnerCallbacks.OnPlayerLeft(NetworkRunner runner, PlayerRef player)
