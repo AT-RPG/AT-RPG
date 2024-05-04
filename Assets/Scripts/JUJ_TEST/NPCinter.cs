98c584af4790e2a8804f84187a08cc60ce2c27e9
ukjin <ukjin32@naver.com>
2024-05-04 22:44:03 +0900

    Map_Fianl_END_commit

--- Assets/Scripts/JUJ_TEST/NPCinter.cs
+++ Assets/Scripts/JUJ_TEST/NPCinter.cs
@@ -11,8 +11,18 @@ public class NPCinter : MonoBehaviour
     public GameObject chatPanel; // 대화하기 패널
     public GameObject storePanel; // 상점 패널
     public Camera zoomCamera; // 상호작용 시 줌 되는 카메라
-    public float newFov;
+    public float newFov; // 상호작용 카메라의 시야값
+    public float targetFov = 60f; // 목표 시야각
+    public float fovChangeSpeed = 5f; // 시야각 변경 속도
+    private Quaternion originalRotation; // NPC 원래 포지션
+    public Animator animator; // NPC 애니메이션
 
+    void Start()
+    {
+        animator.speed = 0f;
+        // NPC의 원래 회전값 저장
+        originalRotation = transform.rotation;
+    }
     public void OnTriggerStay(Collider other)
     {
         if (((1 << other.gameObject.layer) & layer) != 0)
@@ -28,8 +38,14 @@ public class NPCinter : MonoBehaviour
             }
             if (canInter == false && Input.GetKeyDown(KeyCode.F))
             {
-                StartInteraction();
                 ZoomCam();
+                if (zoomCamera != null)
+                {
+                    // NPC가 zoomCamera의 방향을 향하도록 회전
+                    transform.LookAt(zoomCamera.transform);
+                }
+                animator.speed = 1f;
+                StartInteraction();
             }
             // 상호작용 중일 때 상호작용 중지
             else if (canInter == true && Input.GetKeyDown(KeyCode.F))
@@ -39,6 +55,7 @@ public class NPCinter : MonoBehaviour
             }
         }
     }
+
     public void OnTriggerExit(Collider other)
     {
         npcInter.SetActive(false);
@@ -96,11 +113,19 @@ public class NPCinter : MonoBehaviour
         {
             zoomCamera = GetComponent<Camera>();
         }
+        // 카메라의 깊이를 변경하여 우선 순위를 조절합니다.
+        zoomCamera.depth += 3;
+
         Vector3 direction = transform.position - zoomCamera.transform.position;
+        direction.y = 0; // 수직 이동 제거
         Quaternion rotation = Quaternion.LookRotation(direction);
         zoomCamera.transform.rotation = rotation;
-        ChangeFov(newFov);
-        zoomCamera.depth = zoomCamera.depth + 3;
+
+        float currentFov = zoomCamera.fieldOfView;
+        float newFov = Mathf.Lerp(currentFov, targetFov, Time.deltaTime * fovChangeSpeed);
+        zoomCamera.fieldOfView = newFov;
+        
+
     }
     public void ChangeFov(float fov)
     {
@@ -109,6 +134,7 @@ public class NPCinter : MonoBehaviour
     public void ReturnCam()
     {
         ChangeFov(60.0f);
+        transform.rotation = originalRotation;
         zoomCamera.depth = zoomCamera.depth - 3;
     }
 
